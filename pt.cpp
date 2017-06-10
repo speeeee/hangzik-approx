d6cd1aba33cbcc418f9689363fb08a410c78f222
speeeee <snsallay@gmail.com>
2017-06-10 17:42:24 -0400

    now generates sound-file of all N (here, 10) tones that will be played)

--- pt.cpp
+++ pt.cpp
@@ -15,7 +15,7 @@
 #endif
 
 #define	SAMPLE_RATE  44100
-#define	SAMPLE_COUNT (SAMPLE_RATE * 4)	/* 4 seconds */
+#define	SAMPLE_COUNT (SAMPLE_RATE * 10) /* 10 keys played */
 #define	AMPLITUDE    (1.0 * 0x7F000000)
 #define	FREQ         (55.0 / SAMPLE_RATE)
 
@@ -41,7 +41,6 @@ int clampl(int a, int c) { return a<c?c:a; }
 
 int main (void) { SNDFILE *file; srand(time(NULL));
   SF_INFO osfinfo;
-  std::vector<int> buffer(SAMPLE_COUNT);
 
   memset (&osfinfo, 0, sizeof(osfinfo));
 
@@ -56,26 +55,28 @@ int main (void) { SNDFILE *file; srand(time(NULL));
   // very rough idea of what is for later.  this "font" will be used to approximate more complex
   //   signlas.
   std::vector<RSignal> sfnt(10,std::vector<int>(SAMPLE_RATE));
-  for(int i=0;i<10;i++) { Signal a[] = { sin, square }; int e = rand()%10+1;
-    for(int j=0;j<e;j++) { double freq = rand()%560+440; int f = rand()%2;
+  Signal a[] = { sin, square };
+  for(int i=0;i<10;i++) { int e = rand()%10+1;
+    for(int j=0;j<e;j++) { double freq = rand()%945+55; int f = rand()%2;
       for(int q=0;q<SAMPLE_RATE;q++) {
         sfnt[i][q] = AMPLITUDE/e*a[f](2*M_PI*freq/SAMPLE_RATE*q); } } }
 
-  if (osfinfo.channels == 1) {
+  /*if (osfinfo.channels == 1) {
     for(int i = 0, amp = AMPLITUDE; i < SAMPLE_COUNT; i++) {
       // not quite correct for decay of piano.
       // following line modifies 'amp' in rhs.
       //buffer[i] = (amp = clampl(amp-0x7F00,0))/2*(sin(FREQ*2*i*M_PI)+sin(220./SAMPLE_RATE*2*i*M_PI));
-      /*buffer[i] = sin_sum(amp = clampl(amp-0x7F00,0),i
+      buffer[i] = sin_sum(amp = clampl(amp-0x7F00,0),i
         , { [](int q) { return sin(FREQ*2*q*M_PI); },
-            [](int q) { return square(q*FREQ*2*M_PI); } }); } }*/
+            [](int q) { return square(q*FREQ*2*M_PI); } }); } }
       buffer[i] = sfnt[0][i%SAMPLE_RATE]; } }
       //std::vector<Sin> a; for(int t=0;t<10;t++) {
-      //  a.push_back([t](int q) { return sin(440./pow(1.1,t)/SAMPLE_RATE*2*q*M_PI); }); }*/
-      //buffer[i] = sin_sum(amp = clampl(amp-0x7F00,0),i,a); } }
-
-  if (sf_write_int(file, &buffer[0], osfinfo.channels*SAMPLE_COUNT)!=osfinfo.channels*SAMPLE_COUNT) {
-		puts(sf_strerror(file)); }
+      //  a.push_back([t](int q) { return sin(440./pow(1.1,t)/SAMPLE_RATE*2*q*M_PI); }); }
+      //buffer[i] = sin_sum(amp = clampl(amp-0x7F00,0),i,a); } }*/
+  for(int i=0;i<10;i++) {
+    if(sf_write_int(file, &(sfnt[i][0]), osfinfo.channels*SAMPLE_RATE)!=osfinfo.channels*SAMPLE_RATE) {
+      puts(sf_strerror(file)); }
+    /*sf_seek(file,SAMPLE_RATE,SEEK_SET);*/ }
 
   sf_close(file);
   return 0; }
